4751ad34af709b1c1f8456d5d1001b85c69857fe
Gabrielle Brandenburg dos Anjos <gabriellebrandenburg@gmail.com>
2016-11-08 13:27:01 -0200

    add iphone project files

---
+++ AAAHSTEROIDSController/AAAHSTEROIDSController/GameScene.swift
@@ -0,0 +1,126 @@
+//
+//  GameScene.swift
+//  AAAHSTEROIDSController
+//
+//  Created by Gabrielle Brandenburg dos Anjos on 04/11/16.
+//  Copyright © 2016 Gabrielle Brandenburg dos Anjos. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    //TODO: mudar para string:any depois ::: duvida
+    var messageToSend: [String:String] = ["":""]
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    var peerName = String()
+    
+    var counter = 0
+    
+    //var location = ()
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+        // Get label node from scene and store it for use later
+        peerName = appDelegate.mpcManager.peerID.displayName
+        
+        //messageToSend = [peerName:["x":"", "y": ""]]
+        
+        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        if let label = self.label {
+            label.alpha = 0.0
+            label.run(SKAction.fadeIn(withDuration: 2.0))
+        }
+        
+        // Create shape node to use during mouse interaction
+        let w = (self.size.width + self.size.height) * 0.05
+        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        
+        if let spinnyNode = self.spinnyNode {
+            spinnyNode.lineWidth = 2.5
+            
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
+            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+                                              SKAction.fadeOut(withDuration: 0.5),
+                                              SKAction.removeFromParent()]))
+        }
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.green
+            self.addChild(n)
+        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.blue
+            self.addChild(n)
+        }
+        
+        let xPos = String(describing: pos.x)
+        let yPos = String(describing: pos.y)
+        
+        counter += 1
+        
+        print("\n toque \(counter) x: \(xPos) y: \(yPos)")
+        
+        //MARK: COMENTADO PARA TESTAR A POSIÇÃO
+        /*
+        messageToSend = ["sender": peerName, "x": xPos, "y": yPos]
+        
+        //TODO: PRECISA REVISAR!
+        
+        if appDelegate.mpcManager.sendData(dictionaryData: messageToSend, toPeer: appDelegate.mpcManager.session.connectedPeers[0]) {
+            print("message sent")
+        } else {
+            print("error: message not sent")
+        }
+         */
+        
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            self.addChild(n)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let label = self.label {
+            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        }
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
---
+++ AAAHSTEROIDSController/AAAHSTEROIDSController/GameViewController.swift
@@ -0,0 +1,122 @@
+//
+//  GameViewController.swift
+//  AAAHSTEROIDSController
+//
+//  Created by Gabrielle Brandenburg dos Anjos on 04/11/16.
+//  Copyright © 2016 Gabrielle Brandenburg dos Anjos. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+import MultipeerConnectivity
+
+class GameViewController: UIViewController {
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        appDelegate.mpcManager.delegate = self
+        //appDelegate.mpcManager.enableServices(enable: true)
+        
+        loadControllerScene()
+    }
+    
+    func loadControllerScene() {
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
+extension GameViewController: MPCManagerDelegate {
+    
+    func foundPeer() {
+        print("Found Peer!")
+    }
+    
+    
+    func lostPeer() {
+        print("Lost Peer!")
+    }
+    
+
+    func invitationWasReceived(fromPeer: String, codeReceived: String?) {
+        //var textField: UITextField?
+        
+        //criação de um alert view em que o usuario digita o código que aparece na Apple TV para fazer a conexão
+        let alert = UIAlertController(title: "Join a match", message: "Enter the code shown on your Apple TV", preferredStyle: .alert)
+
+        alert.addTextField(configurationHandler: textFieldHandler)
+        
+        let codeTyped = alert.textFields?.first?.text
+        
+        alert.addAction(UIAlertAction(title: "Connect", style: UIAlertActionStyle.default, handler:{ (UIAlertAction) in
+            
+            if codeReceived != nil {
+                if codeTyped == codeReceived {
+                    self.appDelegate.mpcManager.invitationHandler(true, self.appDelegate.mpcManager.session)
+                    print("Started session with \(fromPeer)")
+                }
+                else {
+                    //TODO: implementar um erro decente para código errado
+                    print("ERROR! Wrong code typed. Couldn't start session with \(fromPeer)")
+                    
+                    self.appDelegate.mpcManager.invitationHandler(false, nil)
+                }
+            }
+            
+        }))
+        
+        OperationQueue.main.addOperation { () -> Void in
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func textFieldHandler(textField: UITextField!)
+    {
+        if (textField) != nil {
+            textField.placeholder = "code"
+        }
+    }
+    
+    //quando a conexão é estabelecida, a tela muda para a tela do controle
+    func connectedWithPeer(peerID: MCPeerID) {
+        loadControllerScene()
+    }
+}
